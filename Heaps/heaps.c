6b84b1e4c828b09f5540e527bb56cf925fbdec0e
VangoCode <ronvarshavsky1@gmail.com>
2023-04-30 22:58:51 -0400

    moved around file structure

--- Heaps/heaps.c
+++ Heaps/heaps.c
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include "operations.h"
 
+int A[5] = {0, 0, 0, 0, 0}; // heap for testing purposes
+
 int main() {
     printf("Hello world\n");  
     printf("The parent of index %d is %d\n", 5, parent(5));  
