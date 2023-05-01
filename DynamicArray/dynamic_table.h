788a98565efc08424d8213a8ff40ae1eba141bb3
VangoCode <ronvarshavsky1@gmail.com>
2023-04-30 23:16:41 -0400

    Created prototpyes for dynamic table

---
+++ DynamicArray/dynamic_table.h
@@ -0,0 +1,29 @@
+typedef struct dynamic_table {
+    int size;
+    void *array;
+} DynamicTable;
+
+/*
+ * Initialize an empty dynamic table
+ */
+void initialize_table(DynamicTable *table);
+
+/*
+ * Double table size
+ */
+void double_table_size(DynamicTable *table);
+
+/*
+ * Halve table size
+ */
+void halve_table_size(DynamicTable *table);
+
+/*
+ * Append to a dynamic table
+ */
+void append_to_table(DynamicTable *table);
+
+/*
+ * Pop from a dynamic table
+ */
+void pop_from_table(DynamicTable *table);
