e56f8f6a3c1d0bca6b194a39f937d4801bc6a995
VangoCode <ronvarshavsky1@gmail.com>
2023-05-22 15:44:43 -0400

    created initial SDL window

--- main.c
+++ main.c
@@ -2,8 +2,34 @@
  
 int main(int argc, char **argv)
 {
-    SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Quit();
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        perror("SDL failed to initialize\n");
+        exit(1);
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if (window == NULL) {
+        perror("window creation failed\n");
+        exit(1);
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if (window_surface == NULL) {
+        perror("getting surface from window failed\n");
+        exit(1);
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+
+    // SDL_Quit();
  
-    return 0;
+    // return 0;
 }
