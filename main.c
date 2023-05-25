9511694fccc52bd878cf575d747c8112d8818916
VangoCode <ronvarshavsky1@gmail.com>
2023-05-25 16:05:29 -0400

    added closing event handler to window

--- main.c
+++ main.c
@@ -7,11 +7,11 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+    SDL_Window *window = SDL_CreateWindow("Ron's Algo Visualizer C",
                                           SDL_WINDOWPOS_CENTERED,
                                           SDL_WINDOWPOS_CENTERED,
-                                          680, 480,
-                                          0);
+                                          640, 480,
+                                          SDL_WINDOW_SHOWN);
 
     if (window == NULL) {
         perror("window creation failed\n");
@@ -25,11 +25,26 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    SDL_UpdateWindowSurface(window);
+    // value to check if the window has been closed
+    int is_running = 1;
+    SDL_Event event;
 
-    SDL_Delay(5000);
+    while (is_running == 1) {
+        while (SDL_PollEvent(&event) != 0) {
+            // there is something in the event queue
+            if (event.type == SDL_QUIT) {
+                is_running = 0;
+            }
 
-    // SDL_Quit();
+            SDL_UpdateWindowSurface(window);
+        }
+    }
+
+
+    // SDL_Delay(5000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
  
-    // return 0;
+    return 0;
 }
